3a9a7fc663ddefbd832086c54de69b8c0eb5128a
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-03-07 05:02:00 +0000

    feat: add TextInput component and update Home page layout

--- chat-app/apps/web/app/page.tsx
+++ chat-app/apps/web/app/page.tsx
@@ -1,39 +1,28 @@
-"use client"
-import Image, { type ImageProps } from "next/image";
-
-import styles from "./page.module.css";
-import { TextInput } from '../../../packages/ui/src/textInput';
-import { Button } from '@repo/ui/button';
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
+"use client";
+import { TextInput } from "@repo/ui/input";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className={styles.container}>
-    <h1 className={styles.title}>Welcome to Chat App</h1>
-    
-    <div className={styles.joinForm}>
-  <TextInput size={small} placeholder="Enter your name" />
-      <Button
-      onClick={() => console.log("Join Room")}
-      className={styles.joinButton} appName="web">
-        Join Room
-      </Button>
+    <div style={{
+      height: "100vh",
+      width: "100vw",
+      background: "black",
+      display: "flex",
+      justifyContent: "center",
+      justifyItems: "center"
+    }}>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column"
+      }}>
+        <TextInput size="small" placeholder="Room name"></TextInput>
+        <button onClick={() => {
+          router.push("/chat/123")
+        }}>Join room</button>
+      </div>
     </div>
-  </div>
   );
 }
